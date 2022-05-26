ee77c5924d898060c11d221d4bbd5f7c2ac8a65b
Marlina <Marlinahoro63@gmail.com>
2022-05-26 10:04:05 +0700

    upload file

---
+++ header.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Contoh Modularisasi</title>
+    <link href="style.css" rel="stylesheet" type="text/stylesheet" media="screen" />
+</head>
+
+<body>
+
+    <div class="container">
+        <header>
+            <h1>Modulasi menggunakan Require</h1>
+        </header>
+        <nav>
+            <a href="Home.php">Home</a>
+            <a href="About.php">Tentang</a>
+            <a href="Konak.php">Kontak</a>
+        </nav>
+    </div>
+</body>
+
+</html>
---
+++ home.php
@@ -0,0 +1,8 @@
+<?php require('header.php') ?>
+
+<div class="content">
+    <h2>Ini adalah halaman home</h2>
+    <p>ini adalah bagian content dari halaman.</p>
+</div>
+
+<?php require('footer.php'); ?>
